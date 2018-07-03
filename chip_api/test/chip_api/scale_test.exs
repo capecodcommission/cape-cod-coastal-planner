e43d3747adde1eb4f3cc3450b3312df79c276469
Matt Caldwell <matt.caldwell@timmons.com>
2018-07-03 15:21:42 -0400

    sped up scale_test

--- chip_api/test/chip_api/scale_test.exs
+++ chip_api/test/chip_api/scale_test.exs
@@ -1,24 +1,49 @@
 defmodule ChipApi.ScaleTest do
     use ChipApi.DataCase, async: true
-    alias ChipApi.Adaptation.{Strategies, Scale}
+    alias ChipApi.Adaptation.{Strategies, Scale, Strategy}
 
     @moduletag :scale_case
 
     setup do
-        ChipApi.Seeds.run()
+        scale1 = %Scale{name: "scale1", impact: 1}
+        |> Repo.insert!
+
+        scale2 = %Scale{name: "scale2", impact: 2}
+        |> Repo.insert!
+
+        strat1 = %Strategy{
+            name: "strat1", 
+            description: "desc1",
+            adaptation_categories: [],
+            coastal_hazards: [],
+            impact_scales: [scale1, scale2]
+        }
+        |> Repo.insert!
+
+        strat2 = %Strategy{
+            name: "strat2", 
+            description: "desc2",
+            adaptation_categories: [],
+            coastal_hazards: [],
+            impact_scales: [scale1, scale2]
+        }
+        |> Repo.insert!
+
+        {:ok, data: %{
+            scale1: scale1,
+            scale2: scale2,
+            strat1: strat1,
+            strat2: strat2
+        }}
     end
 
-    test "list_scales returns all impact scales" do
+    test "list_scales returns all impact scales", %{data: data} do
         scales = for c <- Strategies.list_scales(), do: c.name
-        assert ["Site", "Neighborhood", "Community", "Regional"] == scales
+        assert [data.scale1.name, data.scale2.name] == scales
     end
 
-    test "get_scale! with a good id returns the correct scale" do
-        a_scale = from(c in Scale, where: c.name == "Site") |> Repo.one!
-        
-        the_scale = Strategies.get_scale!(a_scale.id)
-
-        assert a_scale == the_scale
+    test "get_scale! with a good id returns the correct scale", %{data: data} do
+        assert data.scale1 == Strategies.get_scale!(data.scale1.id)
     end
 
     test "get_scale! with a bad id raises Ecto.NoResultsError" do
@@ -32,12 +57,8 @@ defmodule ChipApi.ScaleTest do
         end
     end
 
-    test "get_scale with a good id returns the correct scale" do
-        a_scale = from(c in Scale, where: c.name == "Site") |> Repo.one!
-
-        the_scale = Strategies.get_scale(a_scale.id)
-
-        assert a_scale == the_scale
+    test "get_scale with a good id returns the correct scale", %{data: data} do
+       assert data.scale1 == Strategies.get_scale(data.scale1.id)
     end
 
     test "get_scale with a bad id returns nil" do
@@ -71,19 +92,11 @@ defmodule ChipApi.ScaleTest do
         assert "can't be blank" in errors_on(changeset).name
     end
 
-    @attrs %{
-        name: "test",
-        impact: 6
-    }
-    test "create_scale with duplicate name returns error and changeset" do
-        parent = self()
-        task = Task.async(fn ->
-            allow(parent, self())
-            Strategies.create_scale(@attrs)
-        end)
-
-        assert {:ok, _scale} = Task.await(task)
-        assert {:error, changeset} = Strategies.create_scale(@attrs)
+    test "create_scale with duplicate name returns error and changeset", %{data: data} do
+        assert {:error, changeset} = Strategies.create_scale(%{
+            name: data.scale1.name,
+            impact: 6
+        })
         assert "has already been taken" in errors_on(changeset).name
     end
 
@@ -98,13 +111,11 @@ defmodule ChipApi.ScaleTest do
     @attrs %{
         description: "new description"
     }
-    test "update_scale with a good value returns updated scale" do
-        a_scale = from(c in Scale, where: c.name == "Site") |> Repo.one!
-
+    test "update_scale with a good value returns updated scale", %{data: data} do
         parent = self()
         task = Task.async(fn ->
             allow(parent, self())
-            Strategies.update_scale(a_scale, @attrs)
+            Strategies.update_scale(data.scale1, @attrs)
         end)
 
         assert {:ok, scale} = Task.await(task)
@@ -116,40 +127,29 @@ defmodule ChipApi.ScaleTest do
     @attrs %{
         description: 5
     }
-    test "update_scale with a bad description returns error and changeset" do
-        a_scale = from(c in Scale, where: c.name == "Site") |> Repo.one!
-        
-        assert {:error, changeset} = Strategies.update_scale(a_scale, @attrs)
+    test "update_scale with a bad description returns error and changeset", %{data: data} do
+        assert {:error, changeset} = Strategies.update_scale(data.scale1, @attrs)
         assert "is invalid" in errors_on(changeset).description
     end
 
     @attrs %{
         impact: "twenty"
     }
-    test "update_scale with a bad impact returns error and changeset" do
-        a_scale = from(c in Scale, where: c.name == "Site") |> Repo.one!
-
-        assert {:error, changeset} = Strategies.update_scale(a_scale, @attrs)
+    test "update_scale with a bad impact returns error and changeset", %{data: data} do
+        assert {:error, changeset} = Strategies.update_scale(data.scale1, @attrs)
         assert "is invalid" in errors_on(changeset).impact
     end
 
-    @attrs %{
-        name: "Regional"
-    }
-    test "update_scale with to a taken name returns error and changeset" do
-        a_scale = from(c in Scale, where: c.name == "Site") |> Repo.one!
-
-        assert {:error, changeset} = Strategies.update_scale(a_scale, @attrs)
+    test "update_scale with a taken name returns error and changeset", %{data: data} do
+        assert {:error, changeset} = Strategies.update_scale(data.scale1, %{name: data.scale2.name})
         assert "has already been taken" in errors_on(changeset).name
     end
 
-    test "delete_scale with a good value removes the scale" do
-        a_scale = from(c in Scale, where: c.name == "Site") |> Repo.one!
-
+    test "delete_scale with a good value removes the scale", %{data: data} do
         parent = self()
         task = Task.async(fn ->
             allow(parent, self())
-            Strategies.delete_scale(a_scale)
+            Strategies.delete_scale(data.scale2)
         end)
 
         assert {:ok, scale} = Task.await(task)
