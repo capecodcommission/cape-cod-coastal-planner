f11e171edbccdb6f90b7b156c777c7ddaf8cef9f
Matt Caldwell <matt.caldwell@timmons.com>
2018-07-03 15:34:32 -0400

    sped up strategy_test

--- chip_api/test/chip_api/strategy_test.exs
+++ chip_api/test/chip_api/strategy_test.exs
@@ -1,24 +1,65 @@
 defmodule ChipApi.StrategyTest do
     use ChipApi.DataCase, async: true
-    alias ChipApi.Adaptation.{Strategies, Strategy}
+    alias ChipApi.Adaptation.{Strategies, Strategy, Category, Hazard, Scale}
 
     @moduletag :strategy_case
 
     setup do
-        ChipApi.Seeds.run()
+        cat1 = %Category{name: "cat1"}
+        |> Repo.insert!
+
+        cat2 = %Category{name: "cat2"}
+        |> Repo.insert!
+
+        haz1 = %Hazard{name: "haz1"}
+        |> Repo.insert!
+
+        haz2 = %Hazard{name: "haz2"}
+        |> Repo.insert!
+
+        scale1 = %Scale{name: "scale1", impact: 1}
+        |> Repo.insert!
+
+        scale2 = %Scale{name: "scale2", impact: 2}
+        |> Repo.insert!
+
+        strat1 = %Strategy{
+            name: "strat1", 
+            description: "desc1",
+            adaptation_categories: [cat1, cat2],
+            coastal_hazards: [haz1, haz2],
+            impact_scales: [scale1, scale2]
+        }
+        |> Repo.insert!
+
+        strat2 = %Strategy{
+            name: "strat2", 
+            description: "desc2",
+            adaptation_categories: [cat1, cat2],
+            coastal_hazards: [haz1, haz2],
+            impact_scales: [scale1, scale2]
+        }
+        |> Repo.insert!
+
+        {:ok, data: %{
+            cat1: cat1,
+            cat2: cat2,
+            haz1: haz1,
+            haz2: haz2,
+            scale1: scale1,
+            scale2: scale2,
+            strat1: strat1,
+            strat2: strat2
+        }}
     end
 
-    test "list_strategies returns all adaptation strategies" do
+    test "list_strategies returns all adaptation strategies", %{data: data} do
         strategies = for c <- Strategies.list_strategies(), do: c.name
-        assert ["Do Nothing" | _] = strategies
+        assert [data.strat1.name, data.strat2.name] == strategies
     end
 
-    test "get_strategy! with a good id returns the correct strategy" do
-        a_strategy = from(c in Strategy, where: c.name == "Do Nothing") |> Repo.one!
-
-        the_strategy = Strategies.get_strategy!(a_strategy.id)
-
-        assert a_strategy == the_strategy
+    test "get_strategy! with a good id returns the correct strategy", %{data: data} do
+        assert data.strat1.name == Strategies.get_strategy!(data.strat1.id) |> Map.fetch!(:name)
     end
 
     test "get_strategy! with a bad id raises Ecto.NoResultsError" do
@@ -32,12 +73,8 @@ defmodule ChipApi.StrategyTest do
         end
     end
 
-    test "get_strategy with a good id returns the correct category" do
-        a_strategy = from(c in Strategy, where: c.name == "Do Nothing") |> Repo.one!
-
-        the_strategy = Strategies.get_strategy(a_strategy.id)
-
-        assert a_strategy == the_strategy
+    test "get_strategy with a good id returns the correct category", %{data: data} do
+        assert data.strat1.name == Strategies.get_strategy(data.strat1.id) |> Map.fetch!(:name)
     end
 
     test "get_strategy with a bad id returns nil" do
@@ -69,31 +106,19 @@ defmodule ChipApi.StrategyTest do
         assert "can't be blank" in errors_on(changeset).name
     end
 
-    @attrs %{
-        name: "test"
-    }
-    test "create_strategy with duplicate name returns error and changeset" do
-        parent = self()
-        task = Task.async(fn ->
-            allow(parent, self())
-            Strategies.create_strategy(@attrs)
-        end)
-
-        assert {:ok, strategy} = Task.await(task)
-        assert {:error, changeset} = Strategies.create_strategy(@attrs)
+    test "create_strategy with duplicate name returns error and changeset", %{data: data} do
+        assert {:error, changeset} = Strategies.create_strategy(%{name: data.strat1.name})
         assert "has already been taken" in errors_on(changeset).name
     end
 
     @attrs %{
         description: "new description"
     }
-    test "update_strategy with a good value returns updated strategy" do
-        a_strategy = from(c in Strategy, where: c.name == "Do Nothing") |> Repo.one!
-
+    test "update_strategy with a good value returns updated strategy", %{data: data} do
         parent = self()
         task = Task.async(fn ->
             allow(parent, self())
-            Strategies.update_strategy(a_strategy, @attrs)
+            Strategies.update_strategy(data.strat1, @attrs)
         end)
 
         assert {:ok, strategy} = Task.await(task)
@@ -105,30 +130,21 @@ defmodule ChipApi.StrategyTest do
     @attrs %{
         description: 5
     }
-    test "update_strategy with a bad value returns error and changeset" do
-        a_strategy = from(c in Strategy, where: c.name == "Do Nothing") |> Repo.one!
-        
-        assert {:error, changeset} = Strategies.update_strategy(a_strategy, @attrs)
+    test "update_strategy with a bad value returns error and changeset", %{data: data} do
+        assert {:error, changeset} = Strategies.update_strategy(data.strat1, @attrs)
         assert "is invalid" in errors_on(changeset).description
     end
 
-    @attrs %{
-        name: "Undevelopment"
-    }
-    test "update_strategy with a taken name returns error and changeset" do
-        a_strategy = from(c in Strategy, where: c.name == "Do Nothing") |> Repo.one!
-
-        assert {:error, changeset} = Strategies.update_strategy(a_strategy, @attrs)
+    test "update_strategy with a taken name returns error and changeset", %{data: data} do
+        assert {:error, changeset} = Strategies.update_strategy(data.strat1, %{name: data.strat2.name})
         assert "has already been taken" in errors_on(changeset).name
     end
 
-    test "delete_strategy with a good value removes the strategy" do
-        a_strategy = from(c in Strategy, where: c.name == "Do Nothing") |> Repo.one!
-
+    test "delete_strategy with a good value removes the strategy", %{data: data} do
         parent = self()
         task = Task.async(fn ->
             allow(parent, self())
-            Strategies.delete_strategy(a_strategy)
+            Strategies.delete_strategy(data.strat2)
         end)
 
         assert {:ok, strategy} = Task.await(task)
