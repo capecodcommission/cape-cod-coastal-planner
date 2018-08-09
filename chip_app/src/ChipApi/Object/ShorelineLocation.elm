446a6c2ad00a7c54a01b31994552a1c221fd0aac
Matt Caldwell <matt.caldwell@timmons.com>
2018-08-09 11:12:26 -0400

    updated graphql elm types w/ new baseline info

--- chip_app/src/ChipApi/Object/ShorelineLocation.elm
+++ chip_app/src/ChipApi/Object/ShorelineLocation.elm
@@ -25,6 +25,27 @@ selection constructor =
     Object.selection constructor
 
 
+{-| The number of coastal structures in the area
+-}
+coastalStructuresCount : Field Int ChipApi.Object.ShorelineLocation
+coastalStructuresCount =
+    Object.fieldDecoder "coastalStructuresCount" [] Decode.int
+
+
+{-| The number of critical facilities in the area
+-}
+criticalFacilitiesCount : Field Int ChipApi.Object.ShorelineLocation
+criticalFacilitiesCount =
+    Object.fieldDecoder "criticalFacilitiesCount" [] Decode.int
+
+
+{-| The total acreage of eelgrass in the area
+-}
+eelgrassAcres : Field ChipApi.Scalar.Decimal ChipApi.Object.ShorelineLocation
+eelgrassAcres =
+    Object.fieldDecoder "eelgrassAcres" [] (Decode.oneOf [ Decode.string, Decode.float |> Decode.map toString, Decode.int |> Decode.map toString, Decode.bool |> Decode.map toString ] |> Decode.map ChipApi.Scalar.Decimal)
+
+
 {-| The geographic extent of the location
 -}
 extent : Field ChipApi.Scalar.GeographicExtent ChipApi.Object.ShorelineLocation
@@ -93,3 +114,52 @@ minY =
 name : Field String ChipApi.Object.ShorelineLocation
 name =
     Object.fieldDecoder "name" [] Decode.string
+
+
+{-| The number of public beaches in the area
+-}
+publicBeachCount : Field Int ChipApi.Object.ShorelineLocation
+publicBeachCount =
+    Object.fieldDecoder "publicBeachCount" [] Decode.int
+
+
+{-| The number of public buildings in the area
+-}
+publicBuildingsCount : Field Int ChipApi.Object.ShorelineLocation
+publicBuildingsCount =
+    Object.fieldDecoder "publicBuildingsCount" [] Decode.int
+
+
+{-| The total acreage of recreational open space in the area
+-}
+recreationOpenSpaceAcres : Field ChipApi.Scalar.Decimal ChipApi.Object.ShorelineLocation
+recreationOpenSpaceAcres =
+    Object.fieldDecoder "recreationOpenSpaceAcres" [] (Decode.oneOf [ Decode.string, Decode.float |> Decode.map toString, Decode.int |> Decode.map toString, Decode.bool |> Decode.map toString ] |> Decode.map ChipApi.Scalar.Decimal)
+
+
+{-| The total acreage of salt marsh in the area
+-}
+saltMarshAcres : Field ChipApi.Scalar.Decimal ChipApi.Object.ShorelineLocation
+saltMarshAcres =
+    Object.fieldDecoder "saltMarshAcres" [] (Decode.oneOf [ Decode.string, Decode.float |> Decode.map toString, Decode.int |> Decode.map toString, Decode.bool |> Decode.map toString ] |> Decode.map ChipApi.Scalar.Decimal)
+
+
+{-| The total assessed value of property in the area
+-}
+totalAssessedValue : Field ChipApi.Scalar.Decimal ChipApi.Object.ShorelineLocation
+totalAssessedValue =
+    Object.fieldDecoder "totalAssessedValue" [] (Decode.oneOf [ Decode.string, Decode.float |> Decode.map toString, Decode.int |> Decode.map toString, Decode.bool |> Decode.map toString ] |> Decode.map ChipApi.Scalar.Decimal)
+
+
+{-| The number of town ways to water in the area
+-}
+townWaysToWater : Field Int ChipApi.Object.ShorelineLocation
+townWaysToWater =
+    Object.fieldDecoder "townWaysToWater" [] Decode.int
+
+
+{-| Whether or not the location has a working harbor
+-}
+workingHarbor : Field Bool ChipApi.Object.ShorelineLocation
+workingHarbor =
+    Object.fieldDecoder "workingHarbor" [] Decode.bool
