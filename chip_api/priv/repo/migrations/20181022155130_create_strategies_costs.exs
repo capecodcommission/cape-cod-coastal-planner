5c7e7ad3ebe796ec66463962d36cf96215d3942f
Shawn Goulet <shawn.goulet@gmail.com>
2018-10-22 16:25:22 -0400

    added 'create_strategies_life_spans; updated create table code in both 'create_strategies...' table migrations

--- chip_api/priv/repo/migrations/20181022155130_create_strategies_costs.exs
+++ chip_api/priv/repo/migrations/20181022155130_create_strategies_costs.exs
@@ -2,6 +2,9 @@ defmodule ChipApi.Repo.Migrations.CreateStrategiesCosts do
   use Ecto.Migration
 
   def change do
-
+    create table(:strategies_costs, primary_key: false) do
+      add :strategy_id, references(:adaptation_strategies), null: false
+      add :scale_id, references(:impact_costs), null: false
+    end
   end
 end
