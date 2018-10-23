cb8b3ae2e0b473c6ada35e25f44746624c6b712b
Shawn Goulet <shawn.goulet@gmail.com>
2018-10-23 14:12:29 -0400

    updated 'impact_costs' table by removeing 'null: false' from 'name_linear_foot' - don't think this field is actually necessary at this point.

--- chip_api/priv/repo/migrations/20181018144049_create_impact_costs.exs
+++ chip_api/priv/repo/migrations/20181018144049_create_impact_costs.exs
@@ -4,7 +4,7 @@ defmodule ChipApi.Repo.Migrations.CreateImpactCosts do
   def change do
     create table(:impact_costs) do
       add :name, :string, null: false
-      add :name_linear_foot, :string, null: false
+      add :name_linear_foot, :string
       add :description, :text
       add :cost, :integer, null: false
 
